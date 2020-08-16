3e29f629cfe1cabeaefaa9f2614f4b06064164bb
Alexander Dikelsky <alexander.dikelsky@gmail.com>
2020-08-16 07:41:03 -0500

    Trying to figure the printing out

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
+use std::fmt;
+
+use crate::Term::Var;
+use crate::Term::Abs;
+use crate::Term::App;
+
 fn main() {
-    println!("Hello, world!");
+    let y = Abs(Box::new(|x| Var));
+    println!("{}", y.to_string());
 }
+
+enum Term {
+    Var,
+    Abs(Box<dyn Fn(Term) -> Term>),
+    App(Box<dyn Fn(Term) -> Term>, Box<dyn Fn(Term) -> Term>),
+}
+
+impl Term {
+    pub fn to_string(&self) -> String {
+        match &self {
+            Term::Var       => "x".to_string(),
+            Term::Abs(a)    => "λ.".to_string() + a.to_string(),
+            Term::App(a, b) => "y y".to_string(),
+        }
+    }
+}
+
+
+//impl fmt::Display for LatLonPoint {
+//    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+//        write!(f, "Lat: {0:.5}, Lon: {1:.5}", self.phi, self.lambda)
+//    }
+//}
+
