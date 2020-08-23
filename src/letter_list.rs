11e568bfed8bee98d568673f9966d61c5bbd938e
Alexander Dikelsky <alexander.dikelsky@gmail.com>
2020-08-23 16:54:38 -0500

    Add License

--- src/letter_list.rs
+++ src/letter_list.rs
@@ -1,3 +1,15 @@
+//This program is free software: you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation, either version 3 of the License, or
+//(at your option) any later version.
+//
+//This program is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY of FITNESS FOR A PARTICULAR PURPOSE. See the
+//GNU General Public License for more details.
+//
+//You should have recieved a copy of the GNU General Public License
+//along with this program. If not, see <https://www.gnu.org/licenses/>
 pub const LETTERS: [char; 37] = [
         'α','β','ψ','δ','ε','φ','γ','η','ι','ξ','κ','😕',
         'μ','ν','ο','π','q','ρ','σ','τ','θ','ω','🤠', 
